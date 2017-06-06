023f19198e9cd6cdad5acad249222f8af39e084c
Daniel Zwelling <dzwellingmusic@gmail.com>
2017-06-05 22:14:07 -0700

    Hubot additions

--- scripts/ballbag.js
+++ scripts/ballbag.js
@@ -1,14 +1,12 @@
 
-
-
-
-
-
-
-
 module.exports = (robot) => {
-    robot.hear(/hey there/, (msg) => {
-        console.log(msg);
-        msg.reply('Hey is for horses');
+    robot.hear(/hey/, (msg) => {
+        const user = msg.message.user.name;
+        // console.log(msg.message.user);
+        if (user === 'mike') {
+            msg.reply(`${user} is a ballbag`);
+        } else {
+            msg.reply(`${user} is the greatest ever`);
+        }
     });
 };
