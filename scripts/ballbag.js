2a52ea7e1aacb11e41366e1e0d93a9bd4df5b6a6
Daniel Zwelling <dzwellingmusic@gmail.com>
2017-06-14 07:16:33 -0700

    Add gitignore

--- scripts/ballbag.js
+++ scripts/ballbag.js
@@ -1,13 +1,19 @@
 const PythonShell = require('python-shell');
-const pyShell = new PythonShell('./test.py');
+// const pyShell = new PythonShell('test.py');
 // const python = require('child_process');
 
 module.exports = (robot) => {
     robot.hear(/hey/, (res) => {
         const channel = res.envelope.room;
+        const options = {
+            scriptPath: `/danielzwelling/Documents/coding_stuff/hubot/pysheets`,
+        }
         if (channel === 'C5C68HRC6') {
-            PythonShell.run('test.py' (error) => {
-                if (error) {throw error;}
+            PythonShell.run('test.py', (error, results) => {
+                if (error) {
+                    throw error;
+                }
+                console.log(results);
                 console.log('done');
             });
         }
