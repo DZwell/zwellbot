674b105ea5394967e7910bf8c9c621e74d659866
Daniel Zwelling <dzwellingmusic@gmail.com>
2017-06-14 07:15:34 -0700

    Add python script

--- scripts/ballbag.js
+++ scripts/ballbag.js
@@ -1,12 +1,30 @@
+const PythonShell = require('python-shell');
+const pyShell = new PythonShell('./test.py');
+// const python = require('child_process');
 
 module.exports = (robot) => {
-    robot.hear(/hey/, (msg) => {
-        const user = msg.message.user.name;
-        // console.log(msg.message.user);
-        if (user === 'mike') {
-            msg.reply(`${user} is a ballbag`);
-        } else {
-            msg.reply(`${user} is the greatest ever`);
+    robot.hear(/hey/, (res) => {
+        const channel = res.envelope.room;
+        if (channel === 'C5C68HRC6') {
+            PythonShell.run('test.py' (error) => {
+                if (error) {throw error;}
+                console.log('done');
+            });
         }
     });
 };
+
+
+
+
+
+
+            //         python.spawn('python', ['test.py']);
+            // let output;
+            // python.stdout.on('data', (data) => {
+            //     output += data;
+            // });
+
+            // python.on('close', () => {
+            //     console.log(output);
+            // });
---
+++ scripts/test.py
@@ -0,0 +1,1 @@
+print('MyLanta this worked')
